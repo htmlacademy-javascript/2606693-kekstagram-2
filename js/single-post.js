f106568cbd3df667f507cd02f467dad33d373d80
chlvk <ochlvk@gmail.com>
2025-05-14 19:20:10 +0300

    Преобразует модуль с учетом следующего задания

--- js/single-post.js
+++ js/single-post.js
@@ -1,9 +1,21 @@
 import {getArrayItemById, isEscapeKey, createFragment} from './util.js';
 
-const COMMENTS_SHOWN_COUNT = 5;
+const COMMENTS_TO_SHOW_COUNT = 5;
 
 const postElement = document.querySelector('.big-picture');
+const postImageElement = postElement.querySelector('.big-picture__img > img');
+const likesElement = postElement.querySelector('.likes-count');
+const captionElement = postElement.querySelector('.social__caption');
+const commentsCountElement = postElement.querySelector('.social__comment-count');
+const commentsShownElement = postElement.querySelector('.social__comment-shown-count');
+const commentsTotalElement = postElement.querySelector('.social__comment-total-count');
+const commentsLoaderElement = postElement.querySelector('.comments-loader');
+const commentsContainer = postElement.querySelector('.social__comments');
 const closePostElement = postElement.querySelector('.big-picture__cancel');
+const commentTemplate = commentsContainer.querySelector('.social__comment');
+
+let visibleCommentsCount = COMMENTS_TO_SHOW_COUNT;
+let postData = [];
 
 function setSinglePost (postsData) {
   const thumbnailsContainer = document.querySelector('.pictures');
@@ -11,13 +23,18 @@ function setSinglePost (postsData) {
   thumbnailsContainer.addEventListener('click', (evt) => {
     const thumbnailElement = evt.target.closest('.picture');
     if(thumbnailElement) {
-      const postData = getArrayItemById(postsData, thumbnailElement.dataset.id);
-      openPost(postData);
+      postData = getArrayItemById(postsData, thumbnailElement.dataset.id);
+
+      /* <<< Удалить для module8-task2 [Открывается и закрывается (часть 2)] */
+      visibleCommentsCount = postData.comments.length;
+      /* Удалить для module8-task2 [Открывается и закрывается (часть 2)] >>> */
+
+      openPost();
     }
   });
 }
 
-function openPost (postData) {
+function openPost () {
   renderPost(postData);
 
   postElement.classList.remove('hidden');
@@ -30,37 +47,24 @@ function openPost (postData) {
 function closePost () {
   postElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  visibleCommentsCount = COMMENTS_TO_SHOW_COUNT;
 
   closePostElement.removeEventListener('click', onClosePostElementClick);
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
-function renderPost (postData) {
-  const postImageElement = postElement.querySelector('.big-picture__img > img');
-  const likesElement = postElement.querySelector('.likes-count');
-  const captionElement = postElement.querySelector('.social__caption');
-  const commentsCountElement = postElement.querySelector('.social__comment-count');
-  const commentsShownElement = postElement.querySelector('.social__comment-shown-count');
-  const commentsTotalElement = postElement.querySelector('.social__comment-total-count');
-  const commentsLoaderElement = postElement.querySelector('.comments-loader');
-
+function renderPost () {
   postImageElement.src = postData.url;
   likesElement.textContent = postData.likes;
   captionElement.textContent = postData.description;
-  commentsShownElement.textContent = Math.min (COMMENTS_SHOWN_COUNT, postData.comments.length);
-  commentsTotalElement.textContent = postData.comments.length;
 
+  renderCommentsCount();
+  renderCommentsList();
+  renderCommentsLoader();
+
+  /* <<< Удалить для module8-task2 [Открывается и закрывается (часть 2)] */
   commentsCountElement.classList.add('hidden');
-  commentsLoaderElement.classList.add('hidden');
-
-  renderComments(postData.comments);
-}
-
-function renderComments (commentsData) {
-  const commentsContainer = postElement.querySelector('.social__comments');
-  const template = commentsContainer.querySelector('.social__comment');
-  const fragment = createFragment(commentsData, template, renderCommentElement);
-  commentsContainer.replaceChildren(fragment);
+  /* Удалить для module8-task2 [Открывается и закрывается (часть 2)] >>> */
 }
 
 function renderCommentElement (commentsDataItem, template) {
@@ -75,6 +79,32 @@ function renderCommentElement (commentsDataItem, template) {
   return commentElement;
 }
 
+function renderCommentsCount () {
+  commentsShownElement.textContent = Math.min(visibleCommentsCount, postData.comments.length);
+  commentsTotalElement.textContent = postData.comments.length;
+}
+
+function renderCommentsList () {
+  const fragment = createFragment(postData.comments.slice(0, visibleCommentsCount), commentTemplate, renderCommentElement);
+  commentsContainer.replaceChildren(fragment);
+}
+
+function renderCommentsLoader () {
+  if (visibleCommentsCount >= postData.comments.length) {
+    commentsLoaderElement.classList.add('hidden');
+  } else {
+    commentsLoaderElement.classList.remove('hidden');
+    commentsLoaderElement.addEventListener('click', onCommentsLoaderClick, {once: true});
+  }
+}
+
+function onCommentsLoaderClick () {
+  visibleCommentsCount += COMMENTS_TO_SHOW_COUNT;
+  renderCommentsCount();
+  renderCommentsList();
+  renderCommentsLoader();
+}
+
 function onDocumentKeydown (evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -87,4 +117,3 @@ function onClosePostElementClick () {
 }
 
 export {setSinglePost};
-
