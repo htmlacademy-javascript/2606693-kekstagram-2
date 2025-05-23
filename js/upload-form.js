cea52cd4b1f70f687a1e2d0650e78684cd25d2db
chlvk <ochlvk@gmail.com>
2025-05-23 17:19:47 +0300

    Добавляет решение для Помощь друга

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,12 +1,6 @@
 import {toggleClass, isEscapeKey} from './util.js';
-
-const MAX_DESCRIPTION_LENGTH = 140;
-const MAX_HASHTAGS_COUNT = 5;
-
-const HashtagLength = {
-  MIN: 2,
-  MAX: 20
-};
+import {initImageEditor, resetImageEditor} from './image-editor.js';
+import {initValidator} from './form-validator.js';
 
 const uploadFormElement = document.querySelector('.img-upload__form');
 const uploadInputElement = uploadFormElement.querySelector('.img-upload__input');
@@ -14,21 +8,6 @@ const overlayElement = uploadFormElement.querySelector('.img-upload__overlay');
 const hashtagsInputElement = uploadFormElement.querySelector('.text__hashtags');
 const descriptionInputElement = uploadFormElement.querySelector('.text__description');
 const closeOverlayElement = uploadFormElement.querySelector('.img-upload__cancel');
-
-const validationManager = {
-  handleValidation() {
-    return new Pristine(
-      uploadFormElement,
-      {
-        classTo: 'img-upload__field-wrapper',
-        errorClass: 'img-upload__field-wrapper--error',
-        errorTextParent: 'img-upload__field-wrapper',
-      }
-    );
-  },
-  descriptionError: '',
-  hashtagsError: ''
-};
 
 let validationHandler;
 
@@ -46,7 +25,9 @@ const openForm = () => {
 const closeForm = () => {
   uploadInputElement.value = '';
   uploadFormElement.reset();
+
   validationHandler.reset();
+  resetImageEditor();
 
   toggleModal();
 
@@ -75,49 +56,13 @@ const onUploadFormSubmit = (evt) => {
   }
 };
 
-const validateDescription = (value) => {
-  if (value.length > MAX_DESCRIPTION_LENGTH) {
-    validationManager.descriptionError = `Длина комментария должна быть не больше ${MAX_DESCRIPTION_LENGTH} символов`;
-    return false;
-  }
-  return true;
-};
-
-const getDescriptionErrorMessage = () => validationManager.descriptionError;
-
-const validateHashtags = (value) => {
-  const hashtags = value.toLowerCase().trim().split(/\s+/);
-  const hashtagRegexp = /^#[a-zа-яё0-9]{1,19}$/i;
-
-  if (value.length === 0) {
-    return true;
-  }
-
-  switch (true) {
-    case !hashtags.every((hashtag) => hashtagRegexp.test(hashtag)):
-      validationManager.hashtagsError = `Хэштеги начинаются с #, разделены пробелами, длина от ${HashtagLength.MIN} до ${HashtagLength.MAX} символов`;
-      return false;
-    case new Set(hashtags).size !== hashtags.length:
-      validationManager.hashtagsError = 'Один и тот же хэштег не может быть использован дважды';
-      return false;
-    case hashtags.length > MAX_HASHTAGS_COUNT:
-      validationManager.hashtagsError = 'Нельзя указывать больше пяти хэштегов';
-      return false;
-    default:
-      return true;
-  }
-};
-
-const getHashtagsErrorMessage = () => validationManager.hashtagsError;
-
 const initUploadForm = () => {
   uploadInputElement.addEventListener('change', onUploadInputChange);
   closeOverlayElement.addEventListener('click', onCloseOverlayClick);
   uploadFormElement.addEventListener('submit', onUploadFormSubmit);
 
-  validationHandler = validationManager.handleValidation();
-  validationHandler.addValidator(descriptionInputElement, validateDescription, getDescriptionErrorMessage);
-  validationHandler.addValidator(hashtagsInputElement, validateHashtags, getHashtagsErrorMessage);
+  validationHandler = initValidator();
+  initImageEditor();
 };
 
 export {initUploadForm};
